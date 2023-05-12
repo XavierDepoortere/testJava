650c2a42713d01c908e18a2a6bdf3dae31caca4d
XavierDepoortere <depoorx@gmail.com>
2023-05-12 16:00:04 +0200

    Ajout de la premiere class

---
+++ src/fr/eni/TestJavaGit/FirstClassGit.java
@@ -0,0 +1,12 @@
+package fr.eni.TestJavaGit;
+
+public class FirstClassGit {
+	
+	private String mentionsCompl;
+	private int numero;
+	private String complNumero;
+	private String rue;
+	private int cp;
+	private String ville;
+
+}
